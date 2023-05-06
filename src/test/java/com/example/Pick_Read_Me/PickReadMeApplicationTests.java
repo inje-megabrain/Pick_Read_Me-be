9157d643405a0efbd7c62e4d60393f69115ac674
sleeg00 <slee000220@gmail.com>
2023-05-06 15:44:17 +0900

    Start

---
+++ src/test/java/com/example/Pick_Read_Me/PickReadMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Pick_Read_Me;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickReadMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
