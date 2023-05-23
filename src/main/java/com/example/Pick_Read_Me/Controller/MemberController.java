94be33feeb1fc47e9c5fbcbe0cd82bdf88ef8c2c
sleeg00 <slee000220@gmail.com>
2023-05-23 15:01:17 +0900

    리다이렉션

--- src/main/java/com/example/Pick_Read_Me/Controller/MemberController.java
+++ src/main/java/com/example/Pick_Read_Me/Controller/MemberController.java
@@ -3,16 +3,19 @@ package com.example.Pick_Read_Me.Controller;
 
 
 import com.example.Pick_Read_Me.Repository.MemberRepository;
-import io.swagger.annotations.ApiOperation;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
 
 @Log4j2
 @RestController
@@ -23,16 +26,17 @@ public class MemberController {
 
 
     @GetMapping("/home")
-    public String home(HttpServletRequest request, HttpServletResponse response,
-                       @RequestParam("accessToken") String accessToken,
-                       @RequestParam("refreshToken") String refreshToken) {
-        // accessToken과 refreshToken을 활용하여 필요한 작업을 수행하는 로직을 작성합니다.
-        // 예를 들어, 토큰의 유효성 검사, 토큰을 활용한 인증 등을 수행할 수 있습니다.
-        System.out.println("accessToken: " + accessToken);
-        System.out.println("refreshToken: " + refreshToken);
-        // 응답을 생성하여 반환합니다.
-        return "1";
+    public void home(HttpServletRequest request, HttpServletResponse response,
+                     @RequestParam("accessToken") String accessToken,
+                     @RequestParam("refreshToken") String refreshToken) throws IOException {
+        String redirectUrl = "http://localhost:3000/redirect"; // 리다이렉션할 URL
+
+        // accessToken, refreshToken 등 필요한 작업 수행
+        response.setHeader("accessToken", accessToken);
+        response.addHeader("refreshToken", refreshToken);
+        response.sendRedirect(redirectUrl);
     }
+
     @Operation(summary = "test server", description = "서버가 가동중인지 테스트하는 코드입니다.\nToken필요 X")
     @GetMapping("/test")
     public String test() {
--- src/main/java/com/example/Pick_Read_Me/Repository/RefreshRepository.java
+++ src/main/java/com/example/Pick_Read_Me/Repository/RefreshRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 public interface RefreshRepository extends JpaRepository<Refresh, Long> {
 
     /*
-    
+
      */
 }
 
