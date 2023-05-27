a346a06fbec3a4239c80d79807a3ceee5d7cb8da
sleeg00 <slee000220@gmail.com>
2023-05-27 22:38:59 +0900

    Same-Orign Policy

--- src/main/java/com/example/Pick_Read_Me/Config/CorsFilter.java
+++ src/main/java/com/example/Pick_Read_Me/Config/CorsFilter.java
@@ -12,14 +12,15 @@ import java.io.IOException;
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
+
+    @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
-            throws IOException, ServletException
-    {
+            throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
         response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods","*");
+        response.setHeader("Access-Control-Allow-Methods", "*");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers",
                 "Origin, X-Requested-With, Content-Type, Accept, Key, Authorization");
@@ -36,6 +37,6 @@ public class CorsFilter implements Filter {
     }
 
     public void destroy() {
-        //not needed
+        // not needed
     }
 }
