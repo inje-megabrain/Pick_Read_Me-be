a5ed9e103d5e0aa439e3477ea4d9ddfd4a21479f
sleeg00 <slee000220@gmail.com>
2023-05-27 22:51:49 +0900

    Github Cors 추가

--- src/main/java/com/example/Pick_Read_Me/Config/CorsFilter.java
+++ src/main/java/com/example/Pick_Read_Me/Config/CorsFilter.java
@@ -24,6 +24,7 @@ public class CorsFilter implements Filter {
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers",
                 "Origin, X-Requested-With, Content-Type, Accept, Key, Authorization");
+        response.setHeader("Access-Control-Allow-Origin", "https://github.com");
 
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
