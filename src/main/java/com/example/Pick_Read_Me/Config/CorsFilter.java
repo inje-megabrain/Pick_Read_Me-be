4a7ea8c54a0177a23d7d8e456de2c098af843a95
sleeg00 <slee000220@gmail.com>
2023-05-31 00:19:33 +0900

    OAuth 2.0 Config 수정

--- src/main/java/com/example/Pick_Read_Me/Config/CorsFilter.java
+++ src/main/java/com/example/Pick_Read_Me/Config/CorsFilter.java
@@ -23,7 +23,7 @@ public class CorsFilter implements Filter {
         response.setHeader("Access-Control-Allow-Methods", "*");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers",
-                "Origin, X-Requested-With, Content-Type, Accept, Key, Authorization, authorizationsecret, accessToken, refreshToken");
+                "Origin, X-Requested-With, Content-Type, Accept, Key, Authorization, Authorizationsecret, accessToken, refreshToken");
 
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
