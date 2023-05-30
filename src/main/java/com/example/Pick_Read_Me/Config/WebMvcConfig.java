2bc55088dfeabd662cb2fcf3865583d855526db4
sleeg00 <slee000220@gmail.com>
2023-05-30 20:31:37 +0900

    썸네일+Cors

--- src/main/java/com/example/Pick_Read_Me/Config/WebMvcConfig.java
+++ src/main/java/com/example/Pick_Read_Me/Config/WebMvcConfig.java
@@ -7,18 +7,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    // 1시간
     private final long MAX_AGE_SECS = 3600;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry
-                .addMapping("http://localhost:3000")
-                .allowedOriginPatterns("*")
+                .addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "http://52.78.80.150:9000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용되는 Method
                 .allowedHeaders("*") // 허용되는 헤더
                 .allowCredentials(true) // 자격증명 허용
                 .maxAge(MAX_AGE_SECS); // 허용 시간
     }
-
 }
