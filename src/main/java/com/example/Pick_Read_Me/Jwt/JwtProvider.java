9a6470e2dea1a60666740c937a99055d85d7eaeb
sleeg00 <slee000220@gmail.com>
2023-06-05 23:02:44 +0900

    JWT 수정

--- src/main/java/com/example/Pick_Read_Me/Jwt/JwtProvider.java
+++ src/main/java/com/example/Pick_Read_Me/Jwt/JwtProvider.java
@@ -28,8 +28,8 @@ public class JwtProvider implements AuthenticationProvider {
 
     private final MyUserDetailsService userDetailsService;
 
-    public static final long TOKEN_VALIDATION_SECOND = 1800L;   //30분
-    public static final long REFRESH_TOKEN_VALIDATION_TIME = 300L; //5분
+    public static final long TOKEN_VALIDATION_SECOND = 18000;   //30분
+    public static final long REFRESH_TOKEN_VALIDATION_TIME = 180; //5분
 
 
 
